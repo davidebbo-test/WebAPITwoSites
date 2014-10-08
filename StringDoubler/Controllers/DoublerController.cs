292e21844fe39356ecfd430d06dc643ddf92e76c
davidebbo <david.ebbo@microsoft.com>
2014-10-08 16:45:18 -0700

    Have one service call the other

--- StringDoubler/Controllers/DoublerController.cs
+++ StringDoubler/Controllers/DoublerController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace WebAPIFoo.Controllers
@@ -10,8 +12,23 @@ namespace WebAPIFoo.Controllers
     public class DoublerController : ApiController
     {
         // GET api/doubler/abc
-        public string Get(string id)
+        async public Task<string> Get(string id)
         {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:53753/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("/api/upper/" + id);
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new HttpResponseException(response);
+                }
+
+                id = await response.Content.ReadAsAsync<string>();
+            }
+            
             return id + id;
         }
     }
