d2059ade7784761bfcf74d3876659cd593084a2d
davidebbo <david.ebbo@microsoft.com>
2014-10-08 16:12:35 -0700

    Add second service

---
+++ StringDoubler/Controllers/DoublerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIFoo.Controllers
+{
+    public class DoublerController : ApiController
+    {
+        // GET api/doubler/abc
+        public string Get(string id)
+        {
+            return id + id;
+        }
+    }
+}
